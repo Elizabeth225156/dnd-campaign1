39596b01ebd4f7fdddc70054bc4442628ee36b87
Elizabeth225156 <elizabeth.elden@mcvts.org>
2025-11-04 08:37:22 -0500

    other stuff

--- unwritten_campaign/classes/ai.py
+++ unwritten_campaign/classes/ai.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 
-genai.configure(api_key="AIzaSyBQ6BV1hcUNuNNlzGtYFN9mP4PR0tu-tHM")
+genai.configure(api_key="AIzaSyAes7hQyHkallN5qT2APcmECFt-9nd8iVo")
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 class AI:
@@ -9,7 +9,29 @@ class AI:
 
     def response(self, prompt):
         response = model.generate_content(prompt)
-        self.last_response = response.texts
+        self.last_response = response.text
         
         print(self.last_response)
         return self.last_response
+    
+    def generate_backstory(self, character):
+        prompt = f"generate a dnd 5e character backstory with:  + {str(character)}"
+        response = model.generate_content(prompt)
+        self.last_response = response.text
+        print(self.last_response)
+
+    def start_quest(self, prompt):
+        response = model.generate_content(prompt)
+        self.last_response = response.text
+        print(self.last_response)
+
+    def next_chapter(self, prompt):
+        response = model.generate_content(prompt)
+        self.last_response = response.text
+        print(self.last_response)
+
+    def ending_prompt(self, prompt):
+        prompt = "Generate the last section of a dnd quest"
+        response = model.generate_content(prompt)
+        self.last_response = response.text
+        print(self.last_response)
